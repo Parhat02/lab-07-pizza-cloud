5c9eeb261b21fd4c69b98568b4a1308ecced32c7
Ferhat <parhat002@gmail.com>
2024-04-15 23:40:03 +0300

    homepage

--- src/main/java/com/cydeo/controller/HomeController.java
+++ src/main/java/com/cydeo/controller/HomeController.java
@@ -1,8 +1,16 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class HomeController {
 
+    @GetMapping("/home")
+    public String homePage(){
+
+        return "/home";
+    }
 }
